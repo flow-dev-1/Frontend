e74cd509104fa94ed237fdd729865baaeabd19c9
Jossy Joe <jossyojih@gmail.com>
2024-03-25 16:45:48 +0100

    Dispatch user

--- src/components/dashboard/pages/overview/Overview.js
+++ src/components/dashboard/pages/overview/Overview.js
@@ -9,28 +9,27 @@ import CourseCard from '../../reusable/CourseCard';
 import profileImage from '../../../../assets/user-profile-image.png';
 import './overview.css'
 
-
-
-
+import { useDispatch, useSelector } from "react-redux";
 
 export default function IndividualOverview() {
   const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
 
   return (
     <div className="overview">
 
-      
+
       <div className="d-flex align-items-center justify-content-between">
         <div>
-          <h2>Hi Morayo! </h2>
+          <h2>Hi {user?.first_name}! </h2>
           <p>Welcome back to Flow!</p>
         </div>
-        
+
         <img src={profileImage} alt="user Pprofile image" />
       </div>
-      
+
       <div className="browse-all-courses-text container-fluid">
-<p>Browse through all the courses currently available on FLOW.</p>
+        <p>Browse through all the courses currently available on FLOW.</p>
       </div>
       <div className="courses-list row row-cols-1 row-cols-md-3 g-4 mt-3">
         {courses.map(course => (
