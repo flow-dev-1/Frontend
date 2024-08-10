3235813dab815387bc97a868bdf2081c47d0ff88
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-08-10 23:22:42 +0300

    my-course-card

--- src/components/dashboard/pages/my-courses/MyCourses.js
+++ src/components/dashboard/pages/my-courses/MyCourses.js
@@ -1,22 +1,49 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import courses from '../../json-files/CoursesData'
-import CourseCard from '../../reusable/CourseCard'
+import React from 'react'
+import MyCourseCard from '../../reusable/MyCourseCard'
 import { Icon } from '@iconify/react'
-import femaleprofileImage from '../../../../assets/user-profile-image.png'
-import maleprofileImage from '../../../../assets/male-profile-image.png'
 import './course.css'
+import courseOne from '../../../../assets/course1.png'
+import courseTwo from '../../../../assets/course2.png'
+import courseThree from '../../../../assets/course3.png'
 
-import { useSelector } from 'react-redux'
+const courses = [
+  {
+    id: 1,
+    title: 'Max the Explorer Monkey: Growth Mindset',
+    description:
+      'The curriculum combines engaging educational content, interactive activities, and reflective discussions to...',
+    viewed: 1548,
+    likes: 98,
+    progress: 0,
+    image: courseOne, // Replace with actual image path or URL
+  },
+  {
+    id: 2,
+    title: 'Max the Explorer Monkey: Growth Mindset',
+    description:
+      'The curriculum combines engaging educational content, interactive activities, and reflective discussions to...',
+    viewed: 1548,
+    likes: 98,
+    progress: 10,
+    image: courseTwo, // Replace with actual image path or URL
+  },
+  {
+    id: 3,
+    title: 'Max the Explorer Monkey: Growth Mindset',
+    description:
+      'The curriculum combines engaging educational content, interactive activities, and reflective discussions to...',
+    viewed: 1548,
+    likes: 98,
+    progress: 100,
+    image: courseThree, // Replace with actual image path or URL
+  },
+]
 
 export default function MyCourses() {
-  const navigate = useNavigate()
-  const { user } = useSelector((state) => state.user)
-
   return (
-    <div className='overview w-100'>
+    <div className='w-100'>
       <div className='browse-all-courses-text container-fluid'>
-        <p>Browse through all the courses currently available on FLOW.</p>
+        <p>Enrolled Courses</p>
       </div>
 
       <div className='search-bar'>
@@ -63,12 +90,10 @@ export default function MyCourses() {
         </form>
       </div>
 
-      <div className='courses-list row row-cols-1 row-cols-md-3 g-4 '>
-        {courses
-          .filter((course) => !course.enrolled)
-          .map((course) => (
-            <CourseCard key={course.id} course={course} />
-          ))}
+      <div className='courses-list row row-cols-1 row-cols-md-3 g-4'>
+        {courses.map((course) => (
+          <MyCourseCard key={course.id} course={course} />
+        ))}
       </div>
     </div>
   )
---
+++ src/components/dashboard/reusable/MyCourseCard.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react'
+import { Icon } from '@iconify/react'
+import './reusable.css'
+import Modal from 'react-modal'
+
+const MyCourseCard = ({ course }) => {
+  const [modalIsOpen, setIsOpen] = useState(false)
+  const [modalType, setModalType] = useState('')
+
+  const openModal = (modalType) => {
+    setIsOpen(true)
+    setModalType(modalType)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  const handleButtonClick = () => {
+    if (course.progress === 100) {
+      openModal('feedback')
+    } else if (course.progress > 0 && course.progress < 100) {
+      // Code to resume the course
+    } else {
+      // Code to start the course
+    }
+  }
+
+  return (
+    <div style={{ backgroundColor: '#fff' }} className='reusable-course-card'>
+      <div className='course-card' style={{ height: '100%', width: '100%' }}>
+        <div className='course-details'>
+          <img src={course.image} alt='' className='course-image' />
+          <div className='px-3 py-2'>
+            <h3 style={{ color: '#329BD6', fontSize: '24px' }}>
+              Max the Explorer Monkey:
+            </h3>
+            <h3 style={{ fontSize: '24px' }}>{course.title}</h3>
+            {course.subtitle && <h4>{course.subtitle}</h4>}
+            <p>{course.description}</p>
+            <div className='d-flex icons'>
+              <span>
+                <Icon icon='solar:user-linear' />
+                {course.viewed}
+              </span>
+              <span>
+                <Icon icon='mingcute:thumb-up-line' /> {course.likes}%
+              </span>
+              <span>
+                <Icon icon='bytesize:book' /> {course.progress}%
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className='course-card-btn d-flex'>
+          <button
+            style={{
+              backgroundColor: '#fff',
+              border: '1px solid #329BD6',
+              color: '#329BD6',
+              display: 'flex',
+              justifyContent: 'center',
+              gap: '.4rem',
+              padding: '.5rem 8px',
+            }}
+            className='btn card-btn preview'
+            onClick={() => openModal('review')}
+          >
+            <Icon icon='prime:eye' /> Review
+          </button>
+          <button
+            style={{
+              backgroundColor: course.progress === 100 ? '#fff' : '#329BD6',
+              color: course.progress === 100 ? '#50AA50' : '#fff',
+              display: 'flex',
+              justifyContent: 'center',
+              gap: '.4rem',
+              padding: '.5rem 8px',
+              border: course.progress === 100 ? '1px solid #50AA50' : 'none',
+            }}
+            className='btn card-btn start-resume'
+            onClick={handleButtonClick}
+          >
+            {course.progress === 100 ? (
+              <Icon width={25} icon='ph:seal-check-thin' />
+            ) : (
+              <Icon icon='pepicons-print:play-circle' />
+            )}
+            {course.progress === 100
+              ? 'Completed'
+              : course.progress === 0
+              ? 'Start'
+              : 'Resume'}
+          </button>
+          {course.progress >= 0 && (
+            <Icon
+              icon='hugeicons:comment-01'
+              style={{ color: '#329BD6' }}
+              width={50}
+            />
+          )}
+        </div>
+      </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className='custom-modal'
+        overlayClassName='custom-overlay'
+        contentLabel='Example Modal'
+        shouldCloseOnOverlayClick={true}
+      >
+        {modalType === 'review' && (
+          <div>
+            <h2>Course Review</h2>
+            {/* Insert course review content */}
+            <button onClick={closeModal}>Close</button>
+          </div>
+        )}
+        {modalType === 'feedback' && (
+          <div>
+            <h2>Course Feedback</h2>
+            {/* Insert feedback form or content */}
+            <button onClick={closeModal}>Close</button>
+          </div>
+        )}
+      </Modal>
+    </div>
+  )
+}
+
+export default MyCourseCard
