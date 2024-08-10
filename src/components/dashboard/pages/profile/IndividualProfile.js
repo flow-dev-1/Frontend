47d7a6532a51af9cd5028d501a02d880323f6d03
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-08-10 22:05:37 +0300

    individual-profile

--- src/components/dashboard/pages/profile/IndividualProfile.js
+++ src/components/dashboard/pages/profile/IndividualProfile.js
@@ -13,7 +13,8 @@ export default function IndividualProfile() {
   const [modalIsOpen, setIsOpen] = useState(false)
   const { user } = useSelector((state) => state.user)
   const navigate = useNavigate()
-  const openModal = (user) => {
+
+  const openModal = () => {
     setIsOpen(true)
   }
 
@@ -24,14 +25,7 @@ export default function IndividualProfile() {
   return (
     <>
       <div className='individual-profile container-fluid'>
-        <button
-          className='back-button'
-          style={{ marginBottom: '2rem' }}
-          onClick={() => navigate(-1)}
-        >
-          <Icon icon='mingcute:arrow-left-line' width={20} /> Back
-        </button>
-        <div className='user-basic-info' style={{ padding: '1rem 4rem' }}>
+        <div className='user-basic-info' style={{ padding: '1rem' }}>
           <div className='about-user'>
             <div className='profile-img'>
               {user?.gender === 'male' ? (
@@ -50,73 +44,61 @@ export default function IndividualProfile() {
             </div>
 
             <div className='about-user-info mx-4'>
-              <h2>Musa Haruna</h2>
-
-              <div
-                style={{ margin: '1rem 0' }}
-                className='green-spring-div primary'
-              >
-                School Name
-              </div>
-              <p>LGA | STATE</p>
-              <p>Borno</p>
-              <div className='d-flex'>
-                <p>Country</p>
-                <div className='flag mx-2'>
-                  <img
-                    src={flag}
-                    alt='user country flag'
-                    className='flag-img'
-                  />
-                </div>
+              <h2>{user?.name || 'Morayo Ojikutu'}</h2>
+              <div className='user-details'>
+                <div className='green-spring-div primary'>Primary</div>
               </div>
 
-              <div className='user-interests'>
+              <div className='user-details'>
                 <div className='green-spring-div school'>School Name</div>
                 <div className='green-spring-div student'>Student</div>
               </div>
+
+              <p>LGA | STATE</p>
+              <p>
+                NIGERIA{' '}
+                <img src={flag} alt='Nigeria Flag' className='flag-img' />
+              </p>
             </div>
           </div>
+
+          <button className='edit-profile-btn' onClick={openModal}>
+            Edit Profile
+          </button>
         </div>
 
-        <div className='user-other-info '>
-          <p id='p'>
-            <span
-              className='span'
-              style={{ color: '#5CE1E6', fontSize: '14px' }}
-            >
-              Email:{' '}
-            </span>
-            <span id='p'> hmusaharuna@gmail.com </span>
+        <div className='user-other-info'>
+          <p>
+            <span className='label'>Student ID: </span>
+            <span>{user?.id || 'CIS34524'}</span>
           </p>
           <p>
-            <span
-              className='span'
-              style={{ color: '#5CE1E6', fontSize: '14px' }}
-            >
-              D.O.B:{' '}
-            </span>
-            <span id='p'> 22/08/2000 </span>
+            <span className='label'>D.O.B: </span>
+            <span>{user?.dob || '22/08/2000'}</span>
           </p>
           <p>
-            <span
-              className='span'
-              style={{ color: '#5CE1E6', fontSize: '14px' }}
-            >
-              Phone Number:{' '}
-            </span>
-            <span id='p'> +234 803 456 7890</span>
-          </p>
-          <p>
-            <span
-              className='span'
-              style={{ color: '#5CE1E6', fontSize: '14px' }}
-            >
-              Gender:{' '}
-            </span>
-            <span id='p'>Male</span>
+            <span className='label'>Gender: </span>
+            <span>{user?.gender || 'Female'}</span>
           </p>
         </div>
+
+        <div className='user-parent-info'>
+          <h3 style={{ fontSize: '40px' }}>Parent/Guardian Information</h3>
+          <hr className='my-1' />
+          <p>
+            <span className='label'>Full Name: </span>
+            <span>Mrs Jane Doe</span>
+          </p>
+          <p>
+            <span className='label'>Email Address: </span>
+            <span>Janedoe@gmail.com</span>
+          </p>
+          <p>
+            <span className='label'>Phone Number: </span>
+            <span>+2348149878476</span>
+          </p>
+        </div>
+        <hr />
       </div>
 
       <Modal
@@ -124,10 +106,9 @@ export default function IndividualProfile() {
         onRequestClose={closeModal}
         className='custom-modal'
         overlayClassName='custom-overlay'
-        contentLabel='Example Modal'
+        contentLabel='Edit Profile Modal'
         shouldCloseOnOverlayClick={true}
       >
-        {' '}
         <EditProfileModal onClose={closeModal} />
       </Modal>
     </>
