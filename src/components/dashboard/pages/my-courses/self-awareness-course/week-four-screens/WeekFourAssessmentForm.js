cf18662939fa72b54857e5ab534313ca43922810
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-09-04 14:32:51 +0300

    week-four-integration-ui-needs-fixing

--- src/components/dashboard/pages/my-courses/self-awareness-course/week-four-screens/WeekFourAssessmentForm.js
+++ src/components/dashboard/pages/my-courses/self-awareness-course/week-four-screens/WeekFourAssessmentForm.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import '../newcourse.css';
-import checkedImage from '../../../../../../assets/selfawareness-images/checked.png';
-import unCheckedImage from '../../../../../../assets/selfawareness-images/not-checked.png';
-import { toast } from 'react-toastify';
+import React, { useState, useEffect } from 'react'
+import '../newcourse.css'
+import checkedImage from '../../../../../../assets/selfawareness-images/checked.png'
+import unCheckedImage from '../../../../../../assets/selfawareness-images/not-checked.png'
+import { toast } from 'react-toastify'
 
 export default function WeekFourAssessmentForm({ onNext, onBack }) {
   const questionsArray = [
@@ -26,18 +26,18 @@ export default function WeekFourAssessmentForm({ onNext, onBack }) {
     },
     {
       title: 'Match the actions with the values they represent:',
-      questionList: [], // Empty because this question involves matching, not multiple choice
+      questionList: [],
     },
     {
       title: 'Match the scenarios with the appropriate responses:',
-      questionList: [], // Empty because this question involves matching, not multiple choice
+      questionList: [],
     },
-  ];
+  ]
 
   const leftItemsArray = [
     ['Kindness', 'Respect', 'Responsibility'],
-    ['Honesty', 'Empathy', 'Patience']
-  ];
+    ['Honesty', 'Empathy', 'Patience'],
+  ]
 
   const rightItemsArray = [
     [
@@ -46,119 +46,156 @@ export default function WeekFourAssessmentForm({ onNext, onBack }) {
       'Sarah offers to help David with the books.',
     ],
     [
-      "Tom tells the truth about accidentally breaking the vase.",
+      'Tom tells the truth about accidentally breaking the vase.',
       "Jane listens to her friend's problems without interrupting.",
-      "Emily waits patiently for her turn to speak during a group discussion."
-    ]
-  ];
+      'Emily waits patiently for her turn to speak during a group discussion.',
+    ],
+  ]
 
-  const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(1)
   const [answers, setAnswers] = useState(() => {
-    const storedData = localStorage.getItem('week-four-assessment');
+    const storedData = localStorage.getItem('week-four-assessment')
     if (storedData) {
       try {
-        const parsedData = JSON.parse(storedData);
-        return parsedData.assessment?.answers || [];
+        const parsedData = JSON.parse(storedData)
+        return parsedData.assessment?.answers || []
       } catch (e) {
-        console.error('Error parsing local storage data', e);
-        return [];
+        console.error('Error parsing local storage data', e)
+        return []
       }
     }
-    return [];
-  });
+    return []
+  })
 
-  const [matches, setMatches] = useState(() => {
-    const storedData = localStorage.getItem('week-four-assessment');
-    if (storedData) {
-      try {
-        const parsedData = JSON.parse(storedData);
-        return parsedData.assessment?.matches || [];
-      } catch (e) {
-        console.error('Error parsing local storage data', e);
-        return [];
-      }
-    }
-    return [];
-  });
+  const [matchesSet1, setMatchesSet1] = useState([])
+  const [matchesSet2, setMatchesSet2] = useState([])
 
   const handleQuestionCheck = (optionIndex) => {
     setAnswers((prevState) => {
-      const newAnswers = [...prevState];
-      newAnswers[currentIndex - 1] = optionIndex;
-      return newAnswers;
-    });
-  };
+      const newAnswers = [...prevState]
+      newAnswers[currentIndex - 1] = optionIndex
+      return newAnswers
+    })
+  }
 
   const handleNextStepClick = () => {
-    if (currentIndex > 2 && matches.length !== leftItemsArray[currentIndex - 3].length) {
-      toast.error('Please complete the matching before proceeding.');
-      return;
+    if (
+      (currentIndex === 3 && matchesSet1.length !== leftItemsArray[0].length) ||
+      (currentIndex === 4 && matchesSet2.length !== leftItemsArray[1].length)
+    ) {
+      toast.error('Please complete the matching before proceeding.')
+      return
     }
 
     if (answers[currentIndex - 1] === undefined && currentIndex <= 2) {
-      toast.error('Please select an answer before proceeding.');
-      return;
+      toast.error('Please select an answer before proceeding.')
+      return
     }
 
     if (currentIndex < questionsArray.length) {
-      setCurrentIndex(currentIndex + 1);
+      setCurrentIndex(currentIndex + 1)
     } else {
-      onNext();
+      onNext()
     }
-  };
+  }
 
   const handlePreviousStepClick = () => {
     if (currentIndex > 1) {
-      setCurrentIndex(currentIndex - 1);
+      setCurrentIndex(currentIndex - 1)
     } else {
-      onBack();
+      onBack()
     }
-  };
+  }
 
   useEffect(() => {
     const assessmentData = {
       week: 4,
-      assessment: { answers, matches },
-    };
-    localStorage.setItem('week-four-assessment', JSON.stringify(assessmentData));
-    console.log(assessmentData);
-  }, [answers, matches]);
-
-  const colors = ['#FFB6C1', '#ADD8E6', '#90EE90']; // Colors for pairing
-
-  const handleLeftItemClick = (index) => {
-    if (selectedRight !== null) {
-      setMatches((prev) => [
-        ...prev,
-        { left: index, right: selectedRight, color: colors[matches.length % colors.length] },
-      ]);
-      setSelectedLeft(null);
-      setSelectedRight(null);
-    } else {
-      setSelectedLeft(index);
+      assessment: { answers, matchesSet1, matchesSet2 },
     }
-  };
+    localStorage.setItem('week-four-assessment', JSON.stringify(assessmentData))
+    console.log(assessmentData)
+  }, [answers, matchesSet1, matchesSet2])
 
-  const handleRightItemClick = (index) => {
-    if (selectedLeft !== null) {
-      setMatches((prev) => [
+  const colors = ['#FFB6C1', '#ADD8E6', '#90EE90'] // Colors for pairing
+
+  const [selectedLeft1, setSelectedLeft1] = useState(null)
+  const [selectedRight1, setSelectedRight1] = useState(null)
+
+  const [selectedLeft2, setSelectedLeft2] = useState(null)
+  const [selectedRight2, setSelectedRight2] = useState(null)
+
+  const handleLeftItemClick1 = (index) => {
+    if (selectedRight1 !== null) {
+      setMatchesSet1((prev) => [
         ...prev,
-        { left: selectedLeft, right: index, color: colors[matches.length % colors.length] },
-      ]);
-      setSelectedLeft(null);
-      setSelectedRight(null);
+        {
+          left: index,
+          right: selectedRight1,
+          color: colors[matchesSet1.length % colors.length],
+        },
+      ])
+      setSelectedLeft1(null)
+      setSelectedRight1(null)
     } else {
-      setSelectedRight(index);
+      setSelectedLeft1(index)
     }
-  };
+  }
 
-  const [selectedLeft, setSelectedLeft] = useState(null);
-  const [selectedRight, setSelectedRight] = useState(null);
+  const handleRightItemClick1 = (index) => {
+    if (selectedLeft1 !== null) {
+      setMatchesSet1((prev) => [
+        ...prev,
+        {
+          left: selectedLeft1,
+          right: index,
+          color: colors[matchesSet1.length % colors.length],
+        },
+      ])
+      setSelectedLeft1(null)
+      setSelectedRight1(null)
+    } else {
+      setSelectedRight1(index)
+    }
+  }
+
+  const handleLeftItemClick2 = (index) => {
+    if (selectedRight2 !== null) {
+      setMatchesSet2((prev) => [
+        ...prev,
+        {
+          left: index,
+          right: selectedRight2,
+          color: colors[matchesSet2.length % colors.length],
+        },
+      ])
+      setSelectedLeft2(null)
+      setSelectedRight2(null)
+    } else {
+      setSelectedLeft2(index)
+    }
+  }
+
+  const handleRightItemClick2 = (index) => {
+    if (selectedLeft2 !== null) {
+      setMatchesSet2((prev) => [
+        ...prev,
+        {
+          left: selectedLeft2,
+          right: index,
+          color: colors[matchesSet2.length % colors.length],
+        },
+      ])
+      setSelectedLeft2(null)
+      setSelectedRight2(null)
+    } else {
+      setSelectedRight2(index)
+    }
+  }
 
   const renderQuestion = () => {
-    const question = questionsArray[currentIndex - 1];
-    const leftItems = leftItemsArray[currentIndex - 3] || [];
-    const rightItems = rightItemsArray[currentIndex - 3] || [];
+    const question = questionsArray[currentIndex - 1]
+    const leftItems = leftItemsArray[currentIndex - 3] || []
+    const rightItems = rightItemsArray[currentIndex - 3] || []
 
     return (
       <div className='week-two'>
@@ -194,18 +231,24 @@ export default function WeekFourAssessmentForm({ onNext, onBack }) {
               </ul>
             </div>
           )}
-          {currentIndex > 2 && (
+          {currentIndex > 2 && currentIndex === 3 && (
             <div className='match'>
               <div className='matching-container'>
                 {/* Left Column */}
                 <div className='left-column'>
                   {leftItems.map((item, index) => (
                     <div
-                      id={`left-item-${index}`}
+                      id={`left-item-1-${index}`}
                       key={index}
                       className='match-item'
-                      style={{ backgroundColor: getColorForItem(index, 'left') }}
-                      onClick={() => handleLeftItemClick(index)}
+                      style={{
+                        backgroundColor: getColorForItem(
+                          index,
+                          'left',
+                          matchesSet1
+                        ),
+                      }}
+                      onClick={() => handleLeftItemClick1(index)}
                     >
                       {item}
                     </div>
@@ -216,11 +259,64 @@ export default function WeekFourAssessmentForm({ onNext, onBack }) {
                 <div className='right-column'>
                   {rightItems.map((item, index) => (
                     <div
-                      id={`right-item-${index}`}
+                      id={`right-item-1-${index}`}
                       key={index}
                       className='match-item'
-                      style={{ backgroundColor: getColorForItem(index, 'right') }}
-                      onClick={() => handleRightItemClick(index)}
+                      style={{
+                        backgroundColor: getColorForItem(
+                          index,
+                          'right',
+                          matchesSet1
+                        ),
+                      }}
+                      onClick={() => handleRightItemClick1(index)}
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+          {currentIndex > 3 && (
+            <div className='match'>
+              <div className='matching-container'>
+                {/* Left Column */}
+                <div className='left-column'>
+                  {leftItems.map((item, index) => (
+                    <div
+                      id={`left-item-2-${index}`}
+                      key={index}
+                      className='match-item'
+                      style={{
+                        backgroundColor: getColorForItem(
+                          index,
+                          'left',
+                          matchesSet2
+                        ),
+                      }}
+                      onClick={() => handleLeftItemClick2(index)}
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
+
+                {/* Right Column */}
+                <div className='right-column'>
+                  {rightItems.map((item, index) => (
+                    <div
+                      id={`right-item-2-${index}`}
+                      key={index}
+                      className='match-item'
+                      style={{
+                        backgroundColor: getColorForItem(
+                          index,
+                          'right',
+                          matchesSet2
+                        ),
+                      }}
+                      onClick={() => handleRightItemClick2(index)}
                     >
                       {item}
                     </div>
@@ -231,13 +327,13 @@ export default function WeekFourAssessmentForm({ onNext, onBack }) {
           )}
         </div>
       </div>
-    );
-  };
+    )
+  }
 
-  const getColorForItem = (index, side) => {
-    const match = matches.find((match) => match[side] === index);
-    return match ? match.color : '#f0f0f0';
-  };
+  const getColorForItem = (index, side, matches) => {
+    const match = matches.find((match) => match[side] === index)
+    return match ? match.color : '#f0f0f0'
+  }
 
   return (
     <div>
@@ -269,5 +365,5 @@ export default function WeekFourAssessmentForm({ onNext, onBack }) {
         </button>
       </div>
     </div>
-  );
+  )
 }
