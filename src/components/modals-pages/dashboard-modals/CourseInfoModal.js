afcaae8258697b78b9429575bacd6c8edd6fd3c7
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-09-18 19:50:19 +0300

    course-info-modal

--- src/components/modals-pages/dashboard-modals/CourseInfoModal.js
+++ src/components/modals-pages/dashboard-modals/CourseInfoModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Icon } from '@iconify/react'
 import Modal from 'react-modal'
 import PaymentModal from './PaymentModal'
+import dot from '../../../assets/radix-icons--dot-filled.svg'
 
 export default function CourseInfoModal({ course, onClose }) {
   const [modalIsOpen, setIsOpen] = useState(false)
@@ -15,7 +16,7 @@ export default function CourseInfoModal({ course, onClose }) {
 
   return (
     <div className='course-info-modal'>
-      <div className='py-2 px-4'>
+      <div className='py-2 px-4 overflow-y-modal'>
         <div className='course-info-modal-header'>
           <h2
             className='mb-0'
@@ -32,35 +33,104 @@ export default function CourseInfoModal({ course, onClose }) {
           </button>
         </div>
         <hr className='w-100 h-auto mb-2' />
-        <div
-          style={{
-            width: '100%',
-            height: '160px',
-            backgroundColor: '#D9D9D9',
-            paddingLeft: '1rem',
-          }}
-        ></div>
+        <div>
+          <img
+            style={{
+              width: '100%',
+              height: '160px',
+              objectFit: 'cover',
+            }}
+            src={course?.image}
+            alt=''
+          />
+        </div>
         <div className='course-info-modal-body'>
           <p style={{ fontSize: '20px', color: '#275DAD' }}>Course Overview</p>
-          <p style={{ paddingLeft: '.5rem' }}>{course?.description}</p>
-          <div className='mt-4'>
+          <p style={{ paddingLeft: '.5rem' }}>
+            Welcome to your journey of self-awareness and personal growth! Over
+            the next several weeks, we’ll explore key concepts that will help
+            you understand yourself better, and build meaningful relationships.
+            Here’s what you can look forward to:
+          </p>
+          <div className='mt-4 course-objectives'>
             <p style={{ fontSize: '20px', color: '#275DAD' }}>
               Course Objectives
             </p>
-            {/* <div className='objectives'>
+            <div className='objectives-scroll'>
               <ul>
-                {course.objectives.map((objective, index) => (
-                  <li key={index}>
-                    <p className='fw-bold'>
-                      {objective.title
-                        ? `${objective.title}:`
-                        : objective.title}
+                <li>
+                  <div className=' list'>
+                    <img src={dot} />
+                    <p style={{ fontSize: '15px', color: '#275DAD' }}>
+                      Week 1: Introduction to Self-Awareness
                     </p>
-                    {objective.description}
-                  </li>
-                ))}
+                  </div>
+                  <p style={{ paddingLeft: '.5rem' }}>
+                    What We’ll Do: We’ll start by exploring what it means to be
+                    self-aware. You’ll take a personality test to discover your
+                    unique traits and identify whether you align with the colors
+                    blue, green, red, or yellow. This will set the foundation
+                    for understanding how your personality influences your
+                    behavior.
+                  </p>
+                  <br />
+                  <p>
+                    Why It Matters: Knowing yourself is the first step in
+                    personal growth. Understanding your personality will help
+                    you be self-aware better.
+                  </p>
+                </li>
+                <li>
+                  <div className=' list'>
+                    <img src={dot} />
+                    <p style={{ fontSize: '15px', color: '#275DAD' }}>
+                      Week 2: Identifying Strengths And Weaknesses
+                    </p>
+                  </div>
+
+                  <p style={{ paddingLeft: '.5rem' }}>
+                    We’ll focus on identifying your personal strengths and
+                    weaknesses through scenario-based activities.
+                  </p>
+                </li>
+                <li>
+                  <div className=' list'>
+                    <img src={dot} />
+                    <p style={{ fontSize: '15px', color: '#275DAD' }}>
+                      Week 3: Understanding Values
+                    </p>
+                  </div>
+                  <p style={{ paddingLeft: '.5rem' }}>
+                    We’ll dive into the importance of values, how they guide
+                    your decisions, and how to identify your core values.
+                  </p>
+                </li>
+                <li>
+                  <div className=' list'>
+                    <img src={dot} />
+                    <p style={{ fontSize: '15px', color: '#275DAD' }}>
+                      Week 4: Embracing A Growth Mindset
+                    </p>
+                  </div>
+                  <p style={{ paddingLeft: '.5rem' }}>
+                    We’ll explore the concept of a growth mindset and how to
+                    shift from a fixed mindset to a growth mindset.
+                  </p>
+                </li>
+                <li>
+                  <div className=' list'>
+                    <img src={dot} />
+                    <p style={{ fontSize: '15px', color: '#275DAD' }}>
+                      Week 5: Emotional Intelligence
+                    </p>
+                  </div>
+                  <p style={{ paddingLeft: '.5rem', paddingBottom: '1rem' }}>
+                    We’ll learn about emotional intelligence and how to manage
+                    your emotions as well as recognize the emotions of others.
+                  </p>
+                </li>
               </ul>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
