69eae419635626a4ab522751f41fe6a42068b3a2
tosxin-456 <tosinekshally@gmail.com>
2024-09-19 01:03:00 +0100

    fix

--- src/components/dashboard/pages/my-courses/self-awareness-feedback/Hurray.js
+++ src/components/dashboard/pages/my-courses/self-awareness-feedback/Hurray.js
@@ -9,7 +9,7 @@ const HurrayComponent = () => {
             </div>
             <div className="d-flex align-items-center justify-content-around mx-auto mt-5">
             </div>
-            <p>
+            <p className='para'>
                 Congratulations on completing this journey of self-discovery and growth! Over the past weeks, you’ve explored what it means to be self-aware, learned how to identify and leverage your strengths, understood the importance of your values, and embraced a growth mindset. You’ve also gained insights into emotional intelligence, which will help you navigate relationships and challenges with empathy and resilience.
 
                 Remember, self-awareness is an ongoing journey. The skills and knowledge you’ve acquired aren’t just for today—they’re tools you can use throughout your life to achieve your goals, overcome obstacles, and build meaningful connections. Keep reflecting on your experiences, continue to grow, and always stay true to yourself.
