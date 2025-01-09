2c3be0a4a017979eeab6cbbabaa56de24c55578f
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-01-09 23:18:31 +0100

    fixed issues with week content heights

--- src/components/dashboard/pages/my-courses/compassion-course/components/QuestionBox.jsx
+++ src/components/dashboard/pages/my-courses/compassion-course/components/QuestionBox.jsx
@@ -3,10 +3,8 @@ import "./question.css";
 
 function QuestionBox({ children }) {
   return (
-    <div>
-      <div className="custom-border-20 px-5 py-5 bg-worksheet question-box-container h-450px">
-        {children}
-      </div>
+    <div className="custom-border-20 px-5 py-5 bg-worksheet question-box-container h-450px">
+      {children}
     </div>
   );
 }
