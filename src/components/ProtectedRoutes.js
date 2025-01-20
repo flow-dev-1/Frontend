207ff3b4b1ba39c7a216d3da7c27780e0f6d77f7
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-01-20 16:58:34 +0100

    resolve conflicts

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,23 +1,23 @@
-import { Navigate, useNavigate, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import adminService from '../services/api/admin';
-import { adminData, setCode } from '../redux/reducers/adminReducer';
+import { Navigate, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import adminService from "../services/api/admin";
+import { adminData, setCode } from "../redux/reducers/adminReducer";
 
 const ProtectedRoute = ({ children }) => {
   const dispatch = useDispatch();
   const location = useLocation();
   const isAdmin = useSelector(adminData);
   const params = new URLSearchParams(location.search);
-  const token = params.get('token');
-  const courseEnrollmentId = params.get('courseEnrollmentId');
+  const token = params.get("token");
+  const courseEnrollmentId = params.get("courseEnrollmentId");
 
   const removeTokenFromURL = () => {
     // Remove the token parameter from the URL
-    params.delete('token');
-    params.delete('courseEnrollmentId')
+    params.delete("token");
+    params.delete("courseEnrollmentId");
     const newSearch = params.toString();
-    const newURL = `${location.pathname}${newSearch ? `?${newSearch}` : ''}`;
-    window.history.replaceState({}, '', newURL); // Update URL without reloading
+    const newURL = `${location.pathname}${newSearch ? `?${newSearch}` : ""}`;
+    window.history.replaceState({}, "", newURL); // Update URL without reloading
   };
 
   if (token) {
@@ -26,47 +26,43 @@ const ProtectedRoute = ({ children }) => {
     if (isAdmin.isAdmin) {
       // dispatch(setCode(token));
       if (courseEnrollmentId) {
-        sessionStorage.setItem("flow-courseEnrollmentId", courseEnrollmentId)
+        sessionStorage.setItem("flow-courseEnrollmentId", courseEnrollmentId);
       }
       removeTokenFromURL();
-      return children
-
+      return children;
     } else {
-
       const fetchAdminData = async () => {
         try {
           const adminData = await adminService.getMyProfile(token);
           if (adminData.admin) {
             dispatch(setCode(token));
             if (courseEnrollmentId) {
-              sessionStorage.setItem("flow-courseEnrollmentId", courseEnrollmentId)
+              sessionStorage.setItem(
+                "flow-courseEnrollmentId",
+                courseEnrollmentId
+              );
             }
             removeTokenFromURL();
-            return children
+            return children;
           }
-
         } catch (error) {
-          console.error('Error fetching admin data:', error);
+          console.error("Error fetching admin data:", error);
           return <Navigate to="/sign-in" />;
         }
       };
 
       fetchAdminData();
     }
-
   } else {
     // Get the JWT token from local storage
     const auth_token =
-      localStorage.getItem('Flow-Auth-Token') ||
-      (localStorage.getItem('persist:root') &&
-        JSON.parse(JSON.parse(localStorage.getItem('persist:root')).auth)?.token);
+      localStorage.getItem("Flow-Auth-Token") ||
+      (localStorage.getItem("persist:root") &&
+        JSON.parse(JSON.parse(localStorage.getItem("persist:root")).auth)
+          ?.token);
 
     return auth_token ? children : <Navigate to="/sign-in" />;
   }
-
-
-
-
 };
 
 export default ProtectedRoute;
