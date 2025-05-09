21bf690fe577d05c22de4fbf6920c0ba12c095e2
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-05-09 20:32:10 +0100

    made sign in responsive

--- src/components/common-pages/navbar/Navbar.js
+++ src/components/common-pages/navbar/Navbar.js
@@ -1,56 +1,59 @@
 // src/components/common/Navbar.js
 
-import React from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
-import logo from '../../../assets/logo.png'
-import './navbar.css'
+import logo from "../../../assets/logo.png";
+import "./navbar.css";
 
 export default function Navbar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <nav className='navbar'>
-      <div className='container'>
-        <Link to='/' className='navbar-logo'>
-          <img src={logo} alt='' />
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="" />
         </Link>
 
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <Link to='' className='nav-link'>
+        <Icon icon="mdi:menu" className="d-block d-lg-none" width={30} />
+        
+        <ul className="navbar-nav d-none d-lg-flex">
+          <li className="nav-item">
+            <Link to="" className="nav-link">
               Schools
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='' className='nav-link'>
+          <li className="nav-item">
+            <Link to="" className="nav-link">
               Individuals
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='' className='nav-link'>
+          <li className="nav-item">
+            <Link to="" className="nav-link">
               Resources
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='' className='nav-link'>
+          <li className="nav-item">
+            <Link to="" className="nav-link">
               Contact Us
             </Link>
           </li>
-          <li className='nav-item '>
+          <li className="nav-item ">
             <button
-              className='btn navbar-btn light-btn'
+              className="btn navbar-btn light-btn"
               onClick={() => {
-                navigate('/sign-in')
+                navigate("/sign-in");
               }}
             >
               Sign In
             </button>
           </li>
-          <li className='nav-item '>
+          <li className="nav-item ">
             <button
-              className='btn navbar-btn dark-btn'
+              className="btn navbar-btn dark-btn"
               onClick={() => {
-                navigate('/sign-up')
+                navigate("/sign-up");
               }}
             >
               Sign Up
@@ -59,5 +62,5 @@ export default function Navbar() {
         </ul>
       </div>
     </nav>
-  )
+  );
 }
