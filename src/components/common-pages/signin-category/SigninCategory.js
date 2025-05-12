baf643fdd1356dc068dea7b556c42b61f768f0ce
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-05-12 16:04:07 +0100

    made signup category responsive

--- src/components/common-pages/signin-category/SigninCategory.js
+++ src/components/common-pages/signin-category/SigninCategory.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { Icon } from '@iconify/react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
-import './category.css'
-import individualImage from '../../../assets/onboarding-individual.png'
-import schoolImage from '../../../assets/onboarding-school.png'
+import "./category.css";
+import individualImage from "../../../assets/onboarding-individual.png";
+import schoolImage from "../../../assets/onboarding-school.png";
 
 export default function SigninCategory() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div className='signup-category '>
+    <div className="signup-category ">
       {/* <div > */}
 
-      <div className='back-btn'>
-        <Link to='/'>
-          <Icon icon='fa6-solid:arrow-left' className='back-icon' width={30} />
+      <div className="back-btn">
+        <Link to="/">
+          <Icon icon="fa6-solid:arrow-left" className="back-icon" width={30} />
         </Link>
         <h2>Back to Home</h2>
       </div>
 
-      <div className='signup-options'>
-        <div className='signup-option'>
-          <img src={schoolImage} alt='School' className='card-image' />
-          <div className='card-content'>
+      <div className="signup-options">
+        <div className="signup-option">
+          <img src={schoolImage} alt="School" className="card-image" />
+          <div className="card-content">
             <h2> For Schools</h2>
             <button
-              className='btn signup-button'
+              className="btn signup-button"
               onClick={() => {
-                navigate('/sign-in')
+                navigate("/sign-in");
               }}
             >
               Sign In
@@ -36,14 +36,14 @@ export default function SigninCategory() {
           </div>
         </div>
 
-        <div className='signup-option'>
-          <img src={individualImage} alt='School' className='card-image' />
-          <div className='card-content'>
+        <div className="signup-option">
+          <img src={individualImage} alt="School" className="card-image" />
+          <div className="card-content">
             <h2> For Individuals</h2>
             <button
-              className='btn signup-button'
+              className="btn signup-button"
               onClick={() => {
-                navigate('/sign-in')
+                navigate("/sign-in");
               }}
             >
               Sign In
@@ -53,5 +53,5 @@ export default function SigninCategory() {
       </div>
       {/* </div> */}
     </div>
-  )
+  );
 }
