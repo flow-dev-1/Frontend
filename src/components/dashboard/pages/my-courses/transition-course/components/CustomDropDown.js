f290fceef57995172ce54b01105f6824c3e8d172
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-05-16 21:31:48 +0100

    transition course week 6 made responsive

--- src/components/dashboard/pages/my-courses/transition-course/components/CustomDropDown.js
+++ src/components/dashboard/pages/my-courses/transition-course/components/CustomDropDown.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function CustomDropDown({ value, onChange, options = [] }) {
   return (
-    <label className="dropdown-small-input p-3 border-0 dropdown-input-label px-5">
+    <label className="dropdown-small-input p-3 border-0 dropdown-input-label px-5 w-100">
       <select
         className="border-0 bg-transparent border-outline-0 form-control dropdown-small-input"
-        style={{ maxWidth: "100%", fontSize: "20px" }}
+        style={{ maxWidth: "100%", fontSize: "1.25rem" }}
         value={value}
         onChange={(e) => onChange(e.target.value)}
       >
-         <option value=""> </option>
+        <option value=""> </option>
         <option value="" disabled hidden>
           Select an option
         </option>
@@ -22,6 +22,5 @@ function CustomDropDown({ value, onChange, options = [] }) {
     </label>
   );
 }
-
 
 export default CustomDropDown;
