cfec112c8bb7f587f0ffccce5a5506593c415f7e
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-05-16 23:06:03 +0100

    made transitioncourse week 9 responsive

--- src/components/dashboard/pages/my-courses/transition-course/weeks/week9/page2/Page2.jsx
+++ src/components/dashboard/pages/my-courses/transition-course/weeks/week9/page2/Page2.jsx
@@ -5,26 +5,28 @@ import Button from "../../../components/Button";
 import MediumTextBox from "../../../components/MediumTextBox";
 import { selectPageData } from "../../../../../../../../redux/reducers/navigationSlice";
 import { adminData } from "../../../../../../../../redux/reducers/adminReducer";
-import { userAnswer, saveActivity } from "../../../../../../../../redux/reducers/userAnswersReducer";
+import {
+  userAnswer,
+  saveActivity,
+} from "../../../../../../../../redux/reducers/userAnswersReducer";
 import Resilience from "../../../../../../../../assets/Resilience.png";
 
 function WeekFourPage2() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const pageData = useSelector(selectPageData);
   const adminDatas = useSelector(adminData);
   const userAnswers = useSelector(userAnswer);
-  const [myAnswer, setMyAnswer] = useState(userAnswers)
+  const [myAnswer, setMyAnswer] = useState(userAnswers);
   const [errorMessage, setErrorMessage] = useState("");
 
-
   useEffect(() => {
-
-    if (!userAnswers) return
-    const response = userAnswers?.activities?.find(item => (item.page === pageData.id))
-    setMyAnswer(response?.answer ? response.answer : "")
-    return () => { }
-
-  }, [userAnswers])
+    if (!userAnswers) return;
+    const response = userAnswers?.activities?.find(
+      (item) => item.page === pageData.id
+    );
+    setMyAnswer(response?.answer ? response.answer : "");
+    return () => {};
+  }, [userAnswers]);
 
   const saveUserInput = () => {
     if (!adminDatas.isAdmin && !myAnswer) {
@@ -34,40 +36,44 @@ function WeekFourPage2() {
 
     setErrorMessage(""); // Clear error if input is valid
     // Allow flow admin to proceed without input but do not dispatch answer
-    if (adminDatas.isAdmin) return true
-    dispatch(saveActivity({
-      page: pageData.id,
-      answer: myAnswer
-    }))
-    return true
-  }
+    if (adminDatas.isAdmin) return true;
+    dispatch(
+      saveActivity({
+        page: pageData.id,
+        answer: myAnswer,
+      })
+    );
+    return true;
+  };
 
   const handleInputChange = (e) => {
     setErrorMessage("");
-    setMyAnswer(e.target.value)
-  }
+    setMyAnswer(e.target.value);
+  };
 
   return (
     <>
       <QuestionBox>
-        <div className="d-flex gap-3 mb-3">
+        <div className="d-flex gap-3 mb-3  flex-column flex-md-row">
           <h2 className="text-blue fs-1">Question:</h2>
           <h2 className="text-gray fs-1">
             {pageData.question}
-            {pageData.hasImage && <img src={Resilience} alt="Resilience" />}?
+            {pageData.hasImage && (
+              <img
+                src={Resilience}
+                alt="Resilience"
+                className="question-image"
+              />
+            )}
+            ?
           </h2>
         </div>
-        <MediumTextBox
-          handleChange={handleInputChange}
-          value={myAnswer}
-        />
+        <MediumTextBox handleChange={handleInputChange} value={myAnswer} />
       </QuestionBox>
       {errorMessage && <div className="text-danger">{errorMessage}</div>}
-      <div className="d-flex justify-content-center gap-96px mt-4 w-1029px">
+      <div className="d-flex justify-content-center gap-96px mt-4 w-1029px gap-4">
         <Button text="Prev" />
-        <Button text="Next"
-          customOnClick={saveUserInput}
-        />
+        <Button text="Next" customOnClick={saveUserInput} />
       </div>
     </>
   );
