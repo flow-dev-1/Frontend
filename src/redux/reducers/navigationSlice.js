031ceddb1236a76cd7122ee8c296d21704bb4009
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-11-15 13:00:34 +0100

    tot week 2 responsive

--- src/redux/reducers/navigationSlice.js
+++ src/redux/reducers/navigationSlice.js
@@ -109,8 +109,8 @@ const navigationSlice = createSlice({
     setCourse: (state, action) => {
       if (state.currentCourse !== action.payload) {
         state.currentCourse = action.payload;
-        state.currentWeek = 1;
-        state.currentPage = 6;
+        state.currentWeek = 2;
+        state.currentPage = 1;
         state.currentStep = 1;
       }
     },
